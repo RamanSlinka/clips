9ac1f45a4ea5a746613aeca36ff7615b5fc6e4a9
Roman <romuch1986@gmail.com>
2023-07-17 22:19:10 +0200

    Initial commit. Added tailwind to project. Added base template.

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  darkMode: false,  //or "media"  or "class"
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: [],
+}
+
+
+
