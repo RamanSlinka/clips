14920933df328085ab67ac08828e287031da47b0
Roman <romuch1986@gmail.com>
2023-07-19 22:45:54 +0200

    Fixed memory leak.

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, Input, OnInit} from '@angular/core';
 import {ModalService} from "../../srevices/modal.service";
 
 @Component({
@@ -6,15 +6,18 @@ import {ModalService} from "../../srevices/modal.service";
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.css']
 })
-export class ModalComponent implements OnInit{
+export class ModalComponent implements OnInit {
+  @Input() modalID = '';
 
-  constructor(public modal: ModalService) {
+  constructor(public modal: ModalService, public el: ElementRef) {
+
   }
 
   ngOnInit() {
+    document.body.appendChild(this.el.nativeElement)
   }
 
   public closeModal() {
-    this.modal.toggleModal()
+    this.modal.toggleModal(this.modalID)
   }
 }
--- src/app/srevices/modal.service.ts
+++ src/app/srevices/modal.service.ts
@@ -1,19 +1,42 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+
+interface IModal {
+  id: string;
+  visible: boolean
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class ModalService {
 
-  private  visible = false;
-  constructor() { }
+  private modals : IModal[] =  []
 
-  public isModalOpen() {
-    return this.visible
+  constructor() {
   }
 
-  public toggleModal() {
-this.visible = !this.visible
+  register (id: string) {
+    this.modals.push({
+      id, visible: false
+    })
+  }
+
+  unregister (id: string) {
+    this.modals =this.modals.filter(
+      element => element.id !==id
+    )
+  }
+
+  public isModalOpen(id: string) : boolean  {
+    return !!this.modals.find(element => element.id === id)?.visible
+  }
+
+  public toggleModal(id: string) {
+    const modal = this.modals.find(element => element.id === id)
+    if(modal) {
+      modal.visible = !modal.visible
+    }
+    // this.visible = !this.visible
   }
 
 }
