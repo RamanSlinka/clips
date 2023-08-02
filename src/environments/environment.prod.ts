e9c8b3667f64344e989a1099e4178612efac8340
Roman <romuch1986@gmail.com>
2023-08-02 23:28:54 +0200

    Added firebase to project. Created environment files

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment= {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCud1Y_-heS2hQkIJvK1rqyDsyF6oE5z8E",
+    authDomain: "clips-angular-96f2b.firebaseapp.com",
+    projectId: "clips-angular-96f2b",
+    storageBucket: "clips-angular-96f2b.appspot.com",
+    appId: "1:708968592139:web:a1e19a2f44eb9ee48ee80e"
+  }
+};
