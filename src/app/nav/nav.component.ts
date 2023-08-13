870645ec810b540f05d1c7e5a795c4c7cfa6e8cf
Roman <romuch1986@gmail.com>
2023-08-13 19:49:58 +0200

    added logout

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {ModalService} from "../srevices/modal.service";
 import {AuthService} from "../srevices/auth.service";
+import {AngularFireAuth} from "@angular/fire/compat/auth";
 
 @Component({
   selector: 'app-nav',
@@ -11,11 +12,17 @@ export class NavComponent {
 
 
   constructor(public modal: ModalService,
-              public auth: AuthService) {
+              public auth: AuthService,
+              private afAuth: AngularFireAuth) {
   }
 
   public openModal($event: Event) {
     $event.preventDefault()
     this.modal.toggleModal('auth')
+  }
+
+  async logout($event: Event) {
+    $event.preventDefault()
+    await this.afAuth.signOut()
   }
 }
