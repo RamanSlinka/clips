b336da5abde9d6f6b469e2568dbc279fa1131653
Roman <romuch1986@gmail.com>
2023-08-13 19:30:08 +0200

    solved issue with memory leak in modal window

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {AuthService} from "./srevices/auth.service";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import {Component, OnInit} from '@angular/core';
 })
 export class AppComponent  {
 
+  constructor(public auth: AuthService) {
+  }
 }
--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, Input, OnInit} from '@angular/core';
+import {Component, ElementRef, Input, OnDestroy, OnInit} from '@angular/core';
 import {ModalService} from "../../srevices/modal.service";
 
 @Component({
@@ -6,12 +6,16 @@ import {ModalService} from "../../srevices/modal.service";
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.css']
 })
-export class ModalComponent implements OnInit {
+export class ModalComponent implements OnInit, OnDestroy {
   @Input() modalID = '';
 
   constructor(public modal: ModalService, public el: ElementRef) {
 
   }
+
+  ngOnDestroy(): void {
+       document.body.removeChild(this.el.nativeElement)
+    }
 
   ngOnInit() {
     document.body.appendChild(this.el.nativeElement)
