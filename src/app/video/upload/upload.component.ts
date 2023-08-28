272ec705cba2de9d3d9c238b9d81e8f2424083a5
Roman <romuch1986@gmail.com>
2023-08-28 22:56:31 +0200

    Added uploading files + DB firebase ---> video uploaded to firebase

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -1,4 +1,7 @@
 import {Component} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {AngularFireStorage} from "@angular/fire/compat/storage";
+import { v4 as uuid} from 'uuid'
 
 @Component({
   selector: 'app-upload',
@@ -11,6 +14,19 @@ export class UploadComponent {
   public file: File | null = null
   public nextStep = false
 
+
+  title = new FormControl('', [
+    Validators.required,
+    Validators.minLength(3)
+  ])
+
+  uploadForm = new FormGroup({
+    title: this.title
+  })
+
+  constructor(private storage: AngularFireStorage) {
+  }
+
   storeFile($event: Event) {
     this.isDragover = false
     this.file = ($event as DragEvent).dataTransfer?.files.item(0) ?? null
@@ -18,6 +34,18 @@ export class UploadComponent {
     if (!this.file || this.file.type !== 'video/mp4') {
       return
     }
+    this.title.setValue(
+      this.file.name.replace(/\.[^/.]+$/, '')
+    )
     this.nextStep = true
+  }
+
+  uploadFile() {
+    const clipFileName = uuid()
+    const clipPath = `clips/${clipFileName}.mp4`
+
+    this.storage.upload(clipPath, this.file)
+
+    console.log('file uploaded')
   }
 }
