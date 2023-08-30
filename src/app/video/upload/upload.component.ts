a5dc5b5f36e6daf8ab7c528bd4a426995d7f749e
Roman <romuch1986@gmail.com>
2023-08-30 22:51:37 +0200

    Added different alert messages to uploading progress (depending on success or fail) .  + added error handling

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -1,7 +1,8 @@
 import {Component} from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {AngularFireStorage} from "@angular/fire/compat/storage";
-import { v4 as uuid} from 'uuid'
+import {v4 as uuid} from 'uuid'
+import {last} from "rxjs";
 
 @Component({
   selector: 'app-upload',
@@ -20,6 +21,7 @@ export class UploadComponent {
   public inSubmission = false
 
   public percentage = 0
+  public showPercentage = false
 
 
   title = new FormControl('', [
@@ -52,15 +54,31 @@ export class UploadComponent {
     this.alertColor = 'blue';
     this.alertMessage = 'Please wait! Your clip is being uploaded.';
     this.inSubmission = true
+    this.showPercentage = true
 
     const clipFileName = uuid();
     const clipPath = `clips/${clipFileName}.mp4`;
 
-   const task =  this.storage.upload(clipPath, this.file);
-   task.percentageChanges().subscribe(progress => {
-     this.percentage = progress as number / 100
-   })
+    const task = this.storage.upload(clipPath, this.file);
+    task.percentageChanges().subscribe(progress => {
+      this.percentage = progress as number / 100
+    })
 
-    console.log('file uploaded');
+    task.snapshotChanges().pipe(
+      last()
+    ).subscribe({
+      next: (snapshot) => {
+        this.alertColor = 'green';
+        this.alertMessage = 'Success! You clip is now ready to share with the world.';
+        this.showPercentage = false;
+      },
+      error: (error) => {
+        this.alertColor = 'red';
+        this.alertMessage = 'Upload failed ! Please try again later (*and remember max size - 25mb) !';
+        this.inSubmission = true;
+        this.showPercentage = false;
+        console.error(error)
+      }
+    })
   }
 }
