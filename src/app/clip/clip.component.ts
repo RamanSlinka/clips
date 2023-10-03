56bc975a9a3592e28459962b8b34687cca6d8e98
Roman <romuch1986@gmail.com>
2023-10-03 22:57:30 +0200

    work with video player

--- src/app/clip/clip.component.ts
+++ src/app/clip/clip.component.ts
@@ -1,22 +1,43 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {ActivatedRoute, Params} from "@angular/router";
+import IClip from "../models/clip.model";
+import {DatePipe} from "@angular/common";
+
+// import videojs from 'video.js';
 
 @Component({
   selector: 'app-clip',
   templateUrl: './clip.component.html',
-  styleUrls: ['./clip.component.css']
+  styleUrls: ['./clip.component.css'],
+  providers: [DatePipe]
 })
-export class ClipComponent implements OnInit{
+export class ClipComponent implements OnInit {
 
-  public id = ''
+  @ViewChild('videoPlayer', {static: true}) target?: ElementRef
+  public clip?: IClip
+  public src: any
+
+  // player: videojs.Player;
   constructor(public route: ActivatedRoute) {
   }
 
   ngOnInit(): void {
-    this.route.params.subscribe((params: Params) => {
-      this.id = params['id'];
+    // this.player = videojs(this.target?.nativeElement)
+
+    this.route.data.subscribe(data => {
+      this.clip = data['clip'] as IClip
+
+
+      // this.player?.src({
+      //   src: this.clip.url,
+      //   type: 'video/mp4'
+      // })
+
+      this.src = this.clip.url
+
+
     })
 
-    }
+  }
 
 }
