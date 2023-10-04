c45ce2aa9e1202c6100d0d4d612e290ab0693987
Roman <romuch1986@gmail.com>
2023-10-04 20:58:20 +0200

    added functionality "Copy Link"

--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -75,9 +75,21 @@ export class ManageComponent implements OnInit {
     this.clipService.deleteClip(clip)
 
     this.clips.forEach((element, index) => {
-      if(element.docID == clip.docID) {
+      if (element.docID == clip.docID) {
         this.clips.splice(index, 1)
       }
     })
+  }
+
+  async copyToClipboard($event: MouseEvent, docID: string | undefined) {
+    $event.preventDefault()
+
+    if (!docID) {
+      return
+    }
+    const url = `${location.origin}/clip/${docID}`
+    await navigator.clipboard.writeText(url)
+
+    alert('Link Copied!')
   }
 }
